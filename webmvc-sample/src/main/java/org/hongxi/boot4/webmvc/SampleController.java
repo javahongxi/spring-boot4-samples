72982321dc6235d7c0cf44db648adaa465af9aec
javahongxi <javahongxi@qq.com>
2026-07-08 21:29:21 +0800

    rest

--- webmvc-sample/src/main/java/org/hongxi/boot4/webmvc/SampleController.java
+++ webmvc-sample/src/main/java/org/hongxi/boot4/webmvc/SampleController.java
@@ -1,13 +1,27 @@
 package org.hongxi.boot4.webmvc;
 
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SampleController {
 
-    @GetMapping("/hello")
-    public String hello(String name) {
+    private final RestTemplate restTemplate;
+
+    public SampleController(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
         return "Hello " + name;
+    }
+
+    @GetMapping("/rest")
+    public String hi(String name) {
+        return restTemplate.getForObject("http://localhost:8080/hello/{name}", String.class, name);
     }
 }
